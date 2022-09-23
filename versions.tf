34bbc01c7dfa0b1f4a57e421388fa58351b057c7
cklewar@t-online.de <cklewar@t-online.de>
2022-09-24 00:02:31 +0200

    test run v10

---
+++ versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.3.0"
+  cloud {
+    organization = "cklewar"
+
+    workspaces {
+      name = "f5-xc-api-credential-module"
+    }
+  }
+
+  required_providers {
+    f5xc = {
+      source  = "volterraedge/volterra"
+      version = ">= 0.11.12"
+    }
+
+    local = ">= 2.2.3"
+    null  = ">= 3.1.1"
+    http  = ">= 3.1.0"
+  }
+}
