4d7c5daa23e0fd5d9da2328276f82b2013f54464
cklewar <cklewar@t-online.de>
2022-11-25 14:27:50 +0100

    v34

--- versions.tf
+++ versions.tf
@@ -9,13 +9,13 @@ terraform {
   }
 
   required_providers {
-    f5xc = {
-      source  = "volterraedge/volterra"
-      version = ">= 0.11.12"
+    http = {
+      source  = "hashicorp/http"
+      version = ">= 3.1.0"
     }
 
     local = ">= 2.2.3"
     null  = ">= 3.1.1"
-    http  = ">= 3.1.0"
+
   }
 }
