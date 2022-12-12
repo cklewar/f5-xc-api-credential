61f14a1cda57f62a2f43752ec6653e2fe3eca49d
cklewar <cklewar@f5.com>
2022-12-13 00:48:35 +0100

    0.11.16

--- versions.tf
+++ versions.tf
@@ -1,21 +1,21 @@
 terraform {
   required_version = ">= 1.3.0"
   cloud {
-    organization = "cklewar"
+    organization = cklewar"
+    hostname     = "app.terraform.io"
 
     workspaces {
-      name = "f5-xc-api-credential-module"
+      release = ""
     }
   }
-
+  
   required_providers {
-    http = {
-      source  = "hashicorp/http"
-      version = ">= 3.1.0"
+    f5xc = {
+      source = "volterraedge/volterra"
+      version = ">= 0.11.12"
     }
-
     local = ">= 2.2.3"
-    null  = ">= 3.1.1"
-
+    null = ">= 3.1.1"
+    http = ">= 3.1.0"
   }
 }
